889507a0524ce191c8617ac9088bb42787df8e81
Rohan <60407482+Rohan-yadav@users.noreply.github.com>
2022-01-21 16:05:37 -0500

    Update max30102.c

--- max30102.c
+++ max30102.c
@@ -10,107 +10,106 @@
 #define NUM_THREADS 2
 /* create thread argument struct for thr_func() */
 typedef struct _thread_data_t {
- Page 31
- int tid;
- double stuff;
+   
+   int tid;
+   double stuff;
 } thread_data_t;
+
 /* thread function */
 void *thr_func(void *arg) {
- if(wiringPiSetup() == -1){
-printf("setup wiringPi failed !");
-exit(1);
+          if(wiringPiSetup() == -1){
+         printf("setup wiringPi failed !");
+         exit(1);
+      }
+      pinMode( 29, OUTPUT );
+      digitalWrite( 29, HIGH );
+      sleep(1);
+       unsigned char hr[100];
+      int file, count;
+      if ((file = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY))<0) {
+          perror("UART: Failed to open the file.\n");
+          pthread_exit(NULL);
+      }
+      struct termios options; // the termios structure is vital
+      tcgetattr(file, &options); // sets the parameters for the file
+      // Set up the communications options:
+      // 115200 baud, 8-bit, enable receiver, no modem control lines
+      options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
+      options.c_iflag = IGNPAR | ICRNL; // ignore parity errors
+      tcflush(file, TCIFLUSH); // discard file information
+      tcsetattr(file, TCSANOW, &options); // changes occur immmediately
+      //unsigned char transmit[20] = "Hello Raspberry Pi!"; // send string
+
+      sleep(29); // give the Arduino a chance to respond
+      unsigned char receive[100]; // declare a buffer for receiving data
+      printf("<div><h3>Give 30s to respond:/ Thank you for your paitence</div></h3>\n");
+
+      if ((count = read(file, (void*)receive, 100))<0){ //receive data
+           perror("Failed to read from the input\n");
+           pthread_exit(NULL);
+      }
+      else if (count==0) printf("There was no data available to read!\n");
+      else printf("<div><h3>The following was read in [%d]:
+      %s</div></h3></para></html>\n",count,receive);
+      close(file);
+
+
+      digitalWrite( 29, LOW );
+
+       pthread_exit(NULL);
 }
-pinMode( 29, OUTPUT );
-digitalWrite( 29, HIGH );
-sleep(1);
- unsigned char hr[100];
-int file, count;
-if ((file = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY))<0) {
-perror("UART: Failed to open the file.\n");
-pthread_exit(NULL);
-}
-struct termios options; // the termios structure is vital
-tcgetattr(file, &options); // sets the parameters for the file
-// Set up the communications options:
-// 115200 baud, 8-bit, enable receiver, no modem control lines
-options.c_cflag = B115200 | CS8 | CREAD | CLOCAL;
-options.c_iflag = IGNPAR | ICRNL; // ignore parity errors
-tcflush(file, TCIFLUSH); // discard file information
-tcsetattr(file, TCSANOW, &options); // changes occur immmediately
-//unsigned char transmit[20] = "Hello Raspberry Pi!"; // send string
-//if ((count = write(file, &transmit, 20))<0){ // transmit
- Page 32
-//perror("Failed to write to the output\n");
-//return -1;
-//}
-sleep(29); // give the Arduino a chance to respond
-unsigned char receive[100]; // declare a buffer for receiving data
-printf("<div><h3>Give 30s to respond:/ Thank you for your
-paitence</div></h3>\n");
-if ((count = read(file, (void*)receive, 100))<0){ //receive data
-perror("Failed to read from the input\n");
-pthread_exit(NULL);
-}
-else if (count==0) printf("There was no data available to read!\n");
-else printf("<div><h3>The following was read in [%d]:
-%s</div></h3></para></html>\n",count,receive);
-close(file);
-//unsigned char hr = receive[2-4];
-//printf("<div><h3>Heartrate : %s</div></h3>\n",hr[2-4]);
-//printf("%s",hr);
-digitalWrite( 29, LOW );
- pthread_exit(NULL);
-}
+                  
 int main(int argc, char **argv) {
- int policy = SCHED_FIFO;
+ 
+     int policy = SCHED_FIFO;
 
- pthread_t tid;
- pthread_attr_t tattr;
- int ret;
- Page 33
- int newprio = 1;
- struct sched_param param;
+     pthread_t tid;
+     pthread_attr_t tattr;
+     int ret;
+     
+     int newprio = 1;
+     struct sched_param param;
 
- ret = pthread_attr_init (&tattr);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 1 failed \n ");
- return 1;}
+     ret = pthread_attr_init (&tattr);
+     if(ret != 0) {
+       printf("pthread_attr_setinheritsched() 1 failed \n ");
+       return 1;}
 
- ret = pthread_attr_getschedparam (&tattr, &param);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 3 failed \n");
- return 1;}
-
-
- param.sched_priority = newprio;
-
- ret = pthread_attr_setschedpolicy(&tattr, policy);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 5 failed \n");
- return 1;}
+     ret = pthread_attr_getschedparam (&tattr, &param);
+     if(ret != 0) {
+        printf("pthread_attr_setinheritsched() 3 failed \n");
+        return 1;}
 
 
- ret = pthread_attr_setschedparam (&tattr, &param);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 7 failed \n");
- }
- int i, rc;
- /* create a thread_data_t argument array */
- while (1) {
- Page 34
- /* create threads */
+     param.sched_priority = newprio;
+
+     ret = pthread_attr_setschedpolicy(&tattr, policy);
+     if(ret != 0) {
+       printf("pthread_attr_setinheritsched() 5 failed \n");
+       return 1;}
 
 
- if ((ret = pthread_create(&tid, &tattr, thr_func, NULL))) {
- fprintf(stderr, "error: pthread_create, ret: %d\n", ret);
- return EXIT_FAILURE;
- }
+     ret = pthread_attr_setschedparam (&tattr, &param);
+     if(ret != 0) {
+       printf("pthread_attr_setinheritsched() 7 failed \n");
+       }
+     int i, rc;
+     /* create a thread_data_t argument array */
+     while (1) {
+    
+     /* create threads */
 
 
- printf("-------------------------------The End---------------------------\n");
- pthread_join(tid, NULL);
+     if ((ret = pthread_create(&tid, &tattr, thr_func, NULL))) {
+     fprintf(stderr, "error: pthread_create, ret: %d\n", ret);
+     return EXIT_FAILURE;
+     }
 
 
- }
- return EXIT_SUCCESS;
+     printf("-------------------------------The End---------------------------\n");
+     pthread_join(tid, NULL);
+
+
+     }
+     return EXIT_SUCCESS;
 }
