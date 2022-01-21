110dd372dc0a95f0a54f1e7e486eb1304fcb1317
Rohan <60407482+Rohan-yadav@users.noreply.github.com>
2022-01-21 16:08:30 -0500

    Update Servo.c

--- Servo.c
+++ Servo.c
@@ -9,71 +9,73 @@
 #define NUM_THREADS 2
 /* create thread argument struct for thr_func() */
 typedef struct _thread_data_t {
- int tid;
- double stuff;
-} thread_data_t;
+     int tid;
+     double stuff;
+    } thread_data_t;
+
 /* thread function */
-void *thr_func(void *arg) {
-int t;
- printf ("Raspberry Pi wiringPi test program\n");
- wiringPiSetupGpio();
- pinMode (18, PWM_OUTPUT) ;
- pwmSetMode (PWM_MODE_MS);
- pwmSetRange (2000);
- pwmSetClock (192);
- for(t=0;t<=5;t++){
+     void *thr_func(void *arg) {
+     int t;
+      printf ("Raspberry Pi wiringPi test program\n");
+      wiringPiSetupGpio();
+      pinMode (18, PWM_OUTPUT) ;
+      pwmSetMode (PWM_MODE_MS);
+      pwmSetRange (2000);
+      pwmSetClock (192);
+      for(t=0;t<=5;t++){
 
- pwmWrite(18,1);
- usleep(100000);
- pwmWrite(18,210);
+      pwmWrite(18,1);  //setting duty cycle to 1%
+      usleep(100000);
+      pwmWrite(18,210); //setting duty cycle to 25%
 
- usleep(100000);
+      usleep(100000);
 
-}
-pwmWrite(18,0);
+     }
+     pwmWrite(18,0);
  pthread_exit(NULL);
 }
 int main(int argc, char **argv) {
- int policy = SCHED_FIFO;
+   int policy = SCHED_FIFO; //using first in first out policy.
 
- pthread_t tid;
- pthread_attr_t tattr;
- int ret;
- int newprio = 1;
- struct sched_param param;
+   pthread_t tid;
+   pthread_attr_t tattr;
+   int ret;
+   int newprio = 1;
+   struct sched_param param;
 
- ret = pthread_attr_init (&tattr);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 1 failed \n ");
- return 1;}
+   ret = pthread_attr_init (&tattr);
+     if(ret != 0) {
+     printf("pthread_attr_setinheritsched() 1 failed \n ");
+     return 1;}
 
- ret = pthread_attr_getschedparam (&tattr, &param);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 3 failed \n");
- return 1;}
-
-
- param.sched_priority = newprio;
+   ret = pthread_attr_getschedparam (&tattr, &param);
+ 
+   if(ret != 0) {
+      printf("pthread_attr_setinheritsched() 3 failed \n");
+      return 1;}
 
 
- ret = pthread_attr_setschedpolicy(&tattr, policy);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 5 failed \n");
- return 1;}
+   param.sched_priority = newprio;
 
 
- ret = pthread_attr_setschedparam (&tattr, &param);
- if(ret != 0) {
- printf("pthread_attr_setinheritsched() 7 failed \n");
- }
- int i, rc;
- /* create a thread_data_t argument array */
- /* create threads */
+   ret = pthread_attr_setschedpolicy(&tattr, policy);
+   if(ret != 0) {
+     printf("pthread_attr_setinheritsched() 5 failed \n");
+     return 1;}
 
 
- if ((ret = pthread_create(&tid, &tattr, thr_func, NULL))) {
- fprintf(stderr, "error: pthread_create, ret: %d\n", ret);
- return EXIT_FAILURE;
+   ret = pthread_attr_setschedparam (&tattr, &param);
+   if(ret != 0) {
+      printf("pthread_attr_setinheritsched() 7 failed \n");
+   }
+   int i, rc;
+   /* create a thread_data_t argument array */
+   /* create threads */
+
+
+   if ((ret = pthread_create(&tid, &tattr, thr_func, NULL))) {
+   fprintf(stderr, "error: pthread_create, ret: %d\n", ret);
+   return EXIT_FAILURE;
  }
 
 
